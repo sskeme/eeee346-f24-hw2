5ac6e897189c8a3ce4a32b8bc2751147d1c79a87
Sunny Kim <154963758+sskeme@users.noreply.github.com>
2024-09-08 11:51:07 -0400

    Update quadratic.cpp

--- quadratic.cpp
+++ quadratic.cpp
@@ -1,9 +1,9 @@
-double quad(double x, double a, double b, double c)
+double myQuad(double x, double a, double b, double c)
 {
   // TODO: write your code
 }
 
-double quadPrime(double x, double a, double b)
+double myQuadPrime(double x, double a, double b)
 {
   // TODO: write your code
 }
